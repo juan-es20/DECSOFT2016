1e85f217284e2d2e485761612ad609d4c2bd060b
juan <juanes20@hotmail.com>
2016-07-03 12:38:42 -0400

    comit j

--- admin/index.php
+++ admin/index.php
@@ -3,7 +3,8 @@
         <div class="container">
             <center>
                 <br><br><br><br><br>
-                <a class="btn" href="admin_teachers/index.php">Administrar Docentes</a>
+                <a class="btn btn-sm" href="admin_teachers/index.php">Administrar Docentes</a>
+                <a class="btn btn-sm" href="crear_curso">crear curso</a>
                 <br><br>
             </center>
         </div>
